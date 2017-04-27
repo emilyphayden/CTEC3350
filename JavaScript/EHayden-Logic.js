2ce05c509ee77b4810b9d3be6bd84e641e724abe
emilyphayden <emilyhayden@Emilys-MacBook-Pro.local>
2017-04-27 18:48:00 -0500

    committed my website to GitHub

---
+++ JavaScript/EHayden-Logic.js
@@ -0,0 +1,40 @@
+/*
+    Creator: Emily Hayden
+    Date: 04.18.2017
+    Name of File: EHayden-Logic.js
+    Description: Learning Operations, Comparisons, and Equalities
+
+*/
+
+
+/*var value1, value2;
+
+value1 = false;
+value2 = false;
+
+console.log(!value1);
+console.log(!!value1);
+console.log(value1 && value2);
+console.log(value1 || value2);
+
+*/
+
+/*var a, b;
+a = "0";
+b = 0; 
+
+console.log(a==b);
+console.log(a===b);
+
+
+console.log(a!=b);
+
+console.log(a>=b);
+*/
+var question =  "What is Superman's real name?";
+
+var answer = prompt(question);
+
+alert("You answered " + answer);
+
+
